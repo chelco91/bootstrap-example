a88bbce2308ea2e14e16e1816cd8b05b06644aa6
Christopher Collopy <chelco91@gmail.com>
2015-07-27 12:50:24 -0600

    set up bootstrap outline

--- index.php
+++ index.php
@@ -39,23 +39,108 @@
 			<!--begin page header-->
 			<header>
 				<div class="container">
+					<div class="row">
+						<div class="col-xs-4">
+							<img src="./images/top-bar-img.png" alt="Target Media Logo" height="50"/>
+							<h1>Target Media</h1>
+						</div>
+						<div class="col-xs-4"></div>
+						<div class="col-xs-4">
+							<ul class="nav nav-pills pull-right">
+								<li><a class="" href="#">Home</a></li>
+								<li><a class="" href="#">About</a></li>
+								<li><a class="" href="#">Contact</a></li>
+							</ul>
+						</div>
+					</div>
 				</div>
 			</header>
 
 			<!--begin main body content-->
 			<main class="container">
 
+				<div class="jobotron">
+					<div class="container">
+						<h1>Welcome</h1>
+						<p>Brew Guides from Intelligentsia are the coffee industry's answer to B2B whitepapers. Lovely
+							photography precedes step-by-step instructions for a variety of coffee-making tools. Learn how to
+							brew the perfect cup of coffee in a French press, Chemex, V60, and beyond. It's free information
+							that reminds you why Intelligentsia is such a lauded coffee master (we marketers might even say a
+							coffee "thought leader")... and the content makes you want a cup of their coffee ASAP.
+							<a class="btn btn-primary btn-sm" href="#" role="button">Learn more &raquo;</a>
+						</p>
+					</div>
+				</div>
+
 				<!--first content row, full-width-->
 				<div class="row">
-					<div class="col-xs-6"></div>
-					<div class="col-xs-6"></div>
+					<div class="col-xs-4">
+						<p>
+							Birchbox is a popular purveyor of monthly beauty boxes. The brand also runs an excellent content
+							marketing program, with how-to videos of useful beauty tricks (for example, getting beach waves
+							for short hair and how to look great post-flying). They're essentially running a full-scale beauty
+							magazine without the mess of paper and addresses.
+						</p>
+					</div>
+					<div class="col-xs-8">
+						<p>
+							Custom motorcycle and surfboard outlet Deus Ex Machina consistently creates content that goes
+							beyond selling. It delivers a culture. In the brand's own words, "Deus ex Machina is a step bigger
+							than a brand: it’s a culture. Our openness and enthusiasm strike a chord with people, wherever
+							they are... Deus ('day-us') didn’t set out only to sell custom parts and hand- built motorcycles,
+							but to celebrate a culture of creativity."
+						</p>
+						<p>
+							In the brand's latest short film I Had Too Much to Dream Last Night, the film crew follows surfers
+							catching waves and living the dream in a beautiful landscape. The chill, vibey video doesn't try
+							to sell you surfboards or even mention Deus's product lines, but simply reinforces the lifestyle
+							that Deus is all about. To watch the full film, viewers must enter an email address, which is a
+							smart way to deliver high-quality content at a negligible cost to the viewer.
+						</p>
+					</div>
 				</div>
 
 
-				<!--second content row, split-->
+				<!--second content row, full-width-->
 				<div class="row">
-					<div class="col-md-6"></div>
-					<div class="col-md-6"></div>
+					<div class="col-xs-3">
+						<p>
+							Anthropologie frequently features DIY cocktail recipes on their blog, like this one for a coconut
+							cooler. The drink recipes work for two main reasons: they're seasonal and timely (for example, summer
+							refreshers or winter warmers) and they feature unique ingredients and flavors, mimicking the original
+							style of Anthropologie's fashions. Like many examples of the best content marketing, these posts aren't
+							an obvious play for sales; Anthropologie doesn't sell homemade bitters or simple syrup. Instead, the
+							posts position Anthropologie as a trendy, knowledgeable friend who knows a thing or two about drinks—as
+							well as clothes.
+						</p>
+					</div>
+					<div class="col-xs-3">
+						<p>
+							Brew Guides from Intelligentsia are the coffee industry's answer to B2B whitepapers. Lovely
+							photography precedes step-by-step instructions for a variety of coffee-making tools. Learn how to
+							brew the perfect cup of coffee in a French press, Chemex, V60, and beyond. It's free information
+							that reminds you why Intelligentsia is such a lauded coffee master (we marketers might even say a
+							coffee "thought leader")... and the content makes you want a cup of their coffee ASAP.
+						</p>
+					</div>
+					<div class="col-xs-3">
+						<p>
+							It's a simple concept, but Wistia executes to perfection: short, educational videos that teach
+							viewers how to be better video marketers. Each short lesson is a microcosm of some concept within
+							video marketing, including bulleted lists for easier retention of the subject matter. I can think
+							of very few brands that couldn't create a video series like this for their products or services,
+							with minimal resources invested. Check out this example video on concepting fundamentals.
+						</p>
+					</div>
+					<div class="col-xs-3">
+						<p>
+							Businesses of any size can learn something from the OPEN Forum posts by American Express. Posts
+							center around leadership, customer service, marketing, and technology, and they position American
+							Express as a true partner to business owners. Take a look at these examples of currently trending
+							posts on the website, and get a glimpse of the free, valuable ideas being shared by the credit
+							card company.
+						</p>
+					</div>
 				</div>
 
 			</main>
@@ -64,6 +149,26 @@
 		<!--begin footer-->
 		<footer>
 			<div class="container">
+				<div class="row">
+					<div class="col-xs-4">
+						<p>&copy; Company 2014</p>
+					</div>
+					<div class="col-xs-4"></div>
+					<div class="col-xs-2">
+						<ul class="nav">
+							<li><a class="" href="#">Home</a></li>
+							<li><a class="" href="#">About</a></li>
+							<li><a class="" href="#">Contact</a></li>
+						</ul>
+					</div>
+					<div class="col-xs-2">
+						<ul class="nav">
+							<li><a class="" href="#">Home</a></li>
+							<li><a class="" href="#">About</a></li>
+							<li><a class="" href="#">Contact</a></li>
+						</ul>
+					</div>
+				</div>
 			</div>
 		</footer>
 	</body>
