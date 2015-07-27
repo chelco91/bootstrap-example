ed321628b49483c4cf24046cb3e244dbd76e23a3
Christopher Collopy <chelco91@gmail.com>
2015-07-27 11:10:20 -0600

    set up initial index.php file structure

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+		<!-- Bootstrap Latest compiled and minified CSS -->
+		<link type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet"/>
+
+		<!-- Optional Bootstrap theme -->
+		<link type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css" rel="stylesheet"/>
+
+		<!-- LINK TO YOUR CUSTOM CSS FILES HERE -->
+		<link type="text/css" href="css/styles.css" rel="stylesheet"/>
+
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script type="text/javascript" src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script type="text/javascript" src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+		<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
+		<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.13.1/jquery.validate.min.js"></script>
+		<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.12.1/additional-methods.min.js"></script>
+
+		<!-- Latest compiled and minified Bootstrap JavaScript, all compiled plugins included -->
+		<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+		<title>My First Bootstrap Example</title>
+	</head>
+	<body class="sfooter">
+
+		<!--this wrapper div is for the sticky footer-->
+		<div class="sfooter-content">
+
+			<!--begin page header-->
+			<header>
+				<div class="container">
+				</div>
+			</header>
+
+			<!--begin main body content-->
+			<main class="container">
+
+				<!--first content row, full-width-->
+				<div class="row">
+					<div class="col-xs-6"></div>
+					<div class="col-xs-6"></div>
+				</div>
+
+
+				<!--second content row, split-->
+				<div class="row">
+					<div class="col-md-6"></div>
+					<div class="col-md-6"></div>
+				</div>
+
+			</main>
+		</div>
+
+		<!--begin footer-->
+		<footer>
+			<div class="container">
+			</div>
+		</footer>
+	</body>
+</html>
